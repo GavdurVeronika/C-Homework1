b4dde41ae90a8c14e6cd2c530a8e1f7f02c59778
Veronika <gavdur.veronika@gmail.ru>
2024-03-14 22:00:52 +0300

    Make homework.First steps

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.Write("Input firstNumber: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
