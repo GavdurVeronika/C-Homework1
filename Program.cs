9052e64aedbd6871d992d66d5ef59ae84e0758a1
Veronika <gavdur.veronika@gmail.ru>
2024-03-15 14:13:10 +0300

    Finish homework2

--- Program.cs
+++ Program.cs
@@ -1,23 +1,28 @@
 ﻿//  который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
 
-Console.Write("Input firstNumber: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input firstNumber: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input secondNumber: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input secondNumber: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if (firstNumber > secondNumber)
-{
-    System.Console.WriteLine("larger number = " + firstNumber + ", smaller number = " + secondNumber);
- }
+// if (firstNumber > secondNumber)
+// {
+//     System.Console.WriteLine("larger number = " + firstNumber + ", smaller number = " + secondNumber);
+//  }
 
- if (firstNumber == secondNumber)
-{
-    System.Console.WriteLine("numbers are equal");
- }
+//  if (firstNumber == secondNumber)
+// {
+//     System.Console.WriteLine("numbers are equal");
+//  }
 
-else 
+// else 
 
-{
-    System.Console.WriteLine("larger number = " + secondNumber + ", smaller number = " + firstNumber);
- }
+// {
+//     System.Console.WriteLine("larger number = " + secondNumber + ", smaller number = " + firstNumber);
+//  }
+
+//  который принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 
+
