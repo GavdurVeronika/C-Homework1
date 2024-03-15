c6b17a28cb58a45036bfba187a46f5beacf15992
Veronika <gavdur.veronika@gmail.ru>
2024-03-15 13:01:18 +0300

    Make homework.First steps1

--- Program.cs
+++ Program.cs
@@ -4,5 +4,5 @@ Console.WriteLine("Hello, World!");
 Console.Write("Input firstNumber: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input second number: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input secondNumber: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
